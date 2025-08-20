1134fe1c9b02de1149e24f3e64018ca2bdb0add1
Md Atikur Rahman <mdatikur.contact@gmail.com>
2025-08-20 22:38:22 +0600

    read json file

--- 1Pd.py
+++ 1Pd.py
@@ -1,11 +1,5 @@
+
 import pandas as pd
-a= {
-    'colors':[700,800,900],
-    'duration': [1,5,7]
-}
 
-arr =pd.DataFrame(a)
-print(arr.loc[[0,2]])
-
-df= pd.read_csv('academicStress.csv')
-print(df.to_string())
+df= pd.read_json('data.json')
+print(df.head(5))
