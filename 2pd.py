af41b9b4fa196ebb547e16ade346db017d060304
Md Atikur Rahman <mdatikur.contact@gmail.com>
2025-08-21 09:09:14 +0600

    replace null value using meadin

--- 2pd.py
+++ 2pd.py
@@ -1,5 +1,6 @@
 import pandas as pd
 df = pd.read_csv('data.csv')
-df.dropna(inplace= True)
-# df_np= df.fillna(200, inplace= True)
+# df.dropna(inplace= True)
+x= df['Maxpulse'].median()
+df.fillna({'Maxpulse':x}, inplace= True)
 print(df.to_string())
